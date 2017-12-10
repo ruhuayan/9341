6213bbc5dc404f1b9ed385a2addaa4c70a9e8ffb
ruhuayan <ruhuayan@gmail.com>
2017-12-09 20:19:09 -0500

    resize images

--- _include/js/main.js
+++ _include/js/main.js
@@ -77,10 +77,10 @@ jQuery(function ($) {
 			thumb_links: 0,			// Individual thumb links for each slide
 			thumbnail_navigation: 0,			// Thumbnail navigation
 			slides: [			// Slideshow Images
-				{ image: '_include/img/slider-images/image01.jpg', title: '<div class="slide-content"><img src="_include/img/logo2.png" /></div>', thumb: '', url: '' },
-				{ image: '_include/img/slider-images/image02.jpg', title: '<div class="slide-content"><img src="_include/img/logo2.png" /></div>', thumb: '', url: '' },
-				{ image: '_include/img/slider-images/image03.jpg', title: '<div class="slide-content"><img src="_include/img/logo2.png" /></div>', thumb: '', url: '' },
-				{ image: '_include/img/slider-images/image04.jpg', title: '<div class="slide-content"><img src="_include/img/logo2.png" /></div>', thumb: '', url: '' }
+				{ image: '_include/img/slider-images/image01.JPG', title: '<div class="slide-content"><img src="_include/img/logo2.png" /></div>', thumb: '', url: '' },
+				{ image: '_include/img/slider-images/image02.JPG', title: '<div class="slide-content"><img src="_include/img/logo2.png" /></div>', thumb: '', url: '' },
+				{ image: '_include/img/slider-images/image03.JPG', title: '<div class="slide-content"><img src="_include/img/logo2.png" /></div>', thumb: '', url: '' },
+				{ image: '_include/img/slider-images/image04.JPG', title: '<div class="slide-content"><img src="_include/img/logo2.png" /></div>', thumb: '', url: '' }
 			],
 
 			// Theme Options			   
